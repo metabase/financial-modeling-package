931d13f8a8c4f2f06846d7b2a0cdb239e1923038
Nina Thiebaut <123422576+nina-metabase@users.noreply.github.com>
2023-04-18 17:47:18 -0400

    Added Logic to push the URLs to the spreadsheet in Master URLs, I need to implement logic to check if they are already there so for now when you want to run the script you need to clear the Master URls file. All fo the auth is in the shared credentials file. I am still running it in quickstart.py right now because when I migrated it to dap it wasn't working so there's clearly some tweaking I need to do, you can just run it by doing python3 quickstart.py

--- data_products/dap.py
+++ data_products/dap.py
@@ -1,4 +1,19 @@
+from __future__ import print_function
 from pathlib import Path
+import json
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# variables
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SAMPLE_SPREADSHEET_ID = '1O4UR4lq0eyDQZVOhwS8R3j-dwhlxxuiI7OPvBerj0-0'
+SAMPLE_RANGE_NAME = 'Master URLs!A2:C10'
+#this is currently hardcoded and repreents the URLs from metabase along with question title but eventually this will programatic
+tabs_json = {'Self Service ARR': 'https://stats.metabase.com/public/question/4d6bf9dd-d3e3-43fb-ae80-9a1b7674068f.csv', 'Self Service Trials': 'https://stats.metabase.com/public/question/28855a12-c1b2-4616-ae8c-1f512e622ccb.csv'}
 
 
 class DAP:
@@ -15,6 +30,7 @@ class DAP:
         print('TODO: Query the tables and pull in info to configure price/product')
         print('TODO: Created config.yml')
 
+
     def create(self):
         """ Create data products based on configuration file. """
         print('TODO: Read config.yml')
@@ -26,3 +42,68 @@ class DAP:
 
         print('TODO: Creating questions with CSV sharing on')
         print('TODO: Creating GSheet financial model from template')
+
+
+    def create_gsheet(self):
+        """Generate Gsheet tabs based on URLs provided by Metabase """
+
+        # I was trying ot put the sheet information in the credentials file
+
+        # load spreadsheet information from credentials file
+        # with open('.credentials.json') as f:
+        #     credentials = json.load(f)
+        #
+        #     SPREADSHEET_ID = credentials['google_sheet_info']['SAMPLE_SPREADSHEET_ID']
+        #     print(credentials)
+        #     SPREADSHEET_RANGE = credentials['google_sheet_info']['SAMPLE_RANGE_NAME']
+        #     print('sheet2', SPREADSHEET_RANGE)
+
+        print('TODO: Ingest Metabase questions URLs')
+
+        # Authenticate
+        creds = None
+        if os.path.exists('.token.json'):
+            creds = Credentials.from_authorized_user_file('.token.json', SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    '.credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('.token.json', 'w') as token:
+                token.write(creds.to_json())
+
+        # Push URL Values in the Master URL
+        try:
+            service = build('sheets', 'v4', credentials=creds)
+            urls = {
+                'values': []
+            }
+
+            # Loop through the JSON data
+            for key, value in tabs_json.items():
+                urls['values'].append([key, value])
+
+            batch_update_body = {
+                'valueInputOption': 'USER_ENTERED',
+                'data': [{
+                    'range': SAMPLE_RANGE_NAME,
+                    'values': urls['values']
+                }]
+            }
+
+            try:
+                response = service.spreadsheets().values().batchUpdate(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                                                       body=batch_update_body).execute()
+                print('Values updated successfully!')
+
+            except HttpError as error:
+                print(f'An error occurred: {error}')
+
+        except HttpError as err:
+            print(err)
+
+
---
+++ quickstart.py
@@ -0,0 +1,129 @@
+from __future__ import print_function
+
+import os.path
+import json
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+def main():
+
+    # I was trying ot put the sheet information in the credentials file
+
+    # load spreadsheet information from credentials file
+    with open('.credentials.json') as f:
+        credentials = json.load(f)
+
+        SAMPLE_SPREADSHEET_ID = credentials['google_sheet_info']['SAMPLE_SPREADSHEET_ID']
+        print("spreadsheet_ID", credentials['google_sheet_info']['SAMPLE_SPREADSHEET_ID'])
+        SAMPLE_RANGE_NAME = credentials['google_sheet_info']['SAMPLE_RANGE_NAME']
+        print('SAMPLE RANGE NAME', credentials['google_sheet_info']['SAMPLE_RANGE_NAME'])
+        tabs_json = credentials['tabs_json']
+
+    print('TODO: Ingest Metabase questions URLs')
+
+    # Authenticate
+    creds = None
+    if os.path.exists('.token.json'):
+        creds = Credentials.from_authorized_user_file('.token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                '.credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('.token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    # Push URL Values in the Master URL
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+        urls = {
+            'values': []
+        }
+
+        # Loop through the JSON data
+        for key, value in tabs_json.items():
+            urls['values'].append([key, value])
+
+        batch_update_body = {
+            'valueInputOption': 'USER_ENTERED',
+            'data': [{
+                'range': SAMPLE_RANGE_NAME,
+                'values': urls['values']
+            }]
+        }
+
+        try:
+            response = service.spreadsheets().values().batchUpdate(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                                                   body=batch_update_body).execute()
+            print('Values updated successfully!')
+
+        except HttpError as error:
+            print(f'An error occurred: {error}')
+
+    except HttpError as err:
+        print(err)
+
+
+    print('TODO: Create Tabs Dynamically & Import Data')
+
+    #I know how to generate the tab if it does not exist but IMPORTDATA is not working as expected
+
+    # for key in tabs_json:
+    #     sheets = service.spreadsheets().get(spreadsheetId=SAMPLE_SPREADSHEET_ID).execute().get('sheets', [])
+    #     sheet_exists = any(sheet['properties']['title'] == key for sheet in sheets)
+    #
+    #     #if the tab does not exist then generate it
+    #     if sheet_exists == False:
+    #         requests = [
+    #             {
+    #                 'addSheet': {
+    #                     'properties': {
+    #                         'title': key,
+    #                     },
+    #                 },
+    #             },
+    #             {
+    #                 'setDataValidation': {
+    #                     'rule': {
+    #                         'condition': {
+    #                             'type': 'CUSTOM_FORMULA',
+    #                             'values': [
+    #                                 {
+    #                                     'userEnteredValue': f'=IMPORTDATA("{tabs_json[key]}")',
+    #                                 },
+    #                             ],
+    #                         },
+    #                         'inputMessage': 'Imported Data',
+    #                     },
+    #                 },
+    #             },
+    #         ]
+    #
+    #         # Send the batch update request to create the new sheet
+    #         try:
+    #             response = service.spreadsheets().batchUpdate(
+    #                 spreadsheetId=SAMPLE_SPREADSHEET_ID,
+    #                 body={'requests': requests}
+    #             ).execute()
+    #             print(f"Sheet '{key}' with IMPORTDATA URL '{tabs_json[key]}' has been created.")
+    #         except HttpError as error:
+    #             print(f"An error occurred: {error}")
+    #
+    #     #
+    #     if sheet_exists:
+    #         print('TODO: check to see if the URL has changed to update the sheet')
+
+
+if __name__ == '__main__':
+    main()
