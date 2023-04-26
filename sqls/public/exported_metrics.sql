dba468a9886dd28adaf51c835eacbffb4b7a8266
Nga Pui Leung <ngapui@metabase.com>
2023-04-26 21:00:10 +0800

    Fix beginning customer count, churn number negation, metric naming convention, and remove current quarter

--- sqls/public/exported_metrics.sql
+++ sqls/public/exported_metrics.sql
@@ -1,45 +1,43 @@
 with arr as (
-    select
-        quarter
-        , date(quarter_at) as quarter_at
-        , concat(status, ' ARR') as metric
-        , arr as value
-    from {quarterly_arr_and_customers} arr
+  select
+    quarter
+    , quarter_at
+    , concat(status, ' ARR') as metric
+    , arr as value
+  from {quarterly_arr_and_customers} arr
 
 ), customers as (
-    select
-        quarter
-        , date(quarter_at) as quarter_at
-        , concat(status, ' Customers') as metric
-        , customers as value
-    from {quarterly_arr_and_customers} customers
+  select
+    quarter
+    , quarter_at
+    , concat(status, ' customers') as metric
+    , customers as value
+  from {quarterly_arr_and_customers} customers
 
 ), yearly_arr as (
-select
-        quarter
-        , date(quarter_at) as quarter_at
-        , concat(status, ' Customers Y/Y%') as metric
-        , (arr - last_year_arr_value)/coalesce(last_year_arr_value,1)  as value
-    from {quarterly_arr_and_customers} customers
+  select
+    quarter
+    , quarter_at
+    , concat(status, ' customers % YoY growth') as metric
+    , (arr - last_year_arr_value)/coalesce(last_year_arr_value, 1) as value
+  from {quarterly_arr_and_customers} customers
 
 ), yearly_customers as (
-select
-        quarter
-        , date(quarter_at) as quarter_at
-        , concat(status, ' ARR Y/Y%') as metric
-        , (customers - last_year_customer_value)/coalesce(last_year_customer_value,1)  as value
-    from {quarterly_arr_and_customers} customers
+  select
+    quarter
+    , date(quarter_at) as quarter_at
+    , concat(status, ' ARR % YoY growth') as metric
+    , (customers - last_year_customer_value)/coalesce(last_year_customer_value, 1) as value
+  from {quarterly_arr_and_customers} customers
 
 ), final as (
-    select * from arr
-    union all
-    select * from customers
-    union all
-    select * from yearly_arr
-    union all
-    select * from yearly_customers
-
-
+  select * from arr
+  union all
+  select * from customers
+  union all
+  select * from yearly_arr
+  union all
+  select * from yearly_customers
 )
 
 select * from final
