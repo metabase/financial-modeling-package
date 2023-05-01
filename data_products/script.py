9dc9078fb755cacde9ace1e66ac3481e0d0c1c15
Max Zheng <max@metabase.com>
2023-05-01 09:42:43 -0700

    Properly create template tags
    This fixes the problem where viewing the model/question would append an edited hash to the URL.

--- data_products/script.py
+++ data_products/script.py
@@ -16,8 +16,8 @@ def setup():
 
 @cli.command(help='Create data products')
 @click.option('--force', is_flag=True, help='Force the creation of everything by overriding existing if needed')
-@click.option('--model', help='Stop after creating the given model name (e.g. stripe_price). '
-                              'Implies force. Useful for testing')
+@click.option('-m', '--model', help='Stop after creating the given model name (e.g. stripe_price). '
+                                    'Implies force. Useful for testing')
 def create(force, model):
     if model:
         force = True
